c2474a6eb80da862c16f999f2fbb5098ff870e3d
sergiosergij <sergiosergij@gmail.com>
2020-05-14 17:05:33 +0300

    Add actionmailer and mailcatcher(working)

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-	default from: 'notifications@example.com'
+	default from: 'libraryoflife@library.com'
 	def welcome_email
   	@user = params[:user]
-  	@url  = 'http://localhost:3000/users/sign_up'
+  	@url  = 'http://localhost:3000/users/sign_in'
   	mail(to: @user.email, subject: 'Welcome to My Awesome Site')
 	end
             
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root "books#index"
   get "books/author" 
   resources :books
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
